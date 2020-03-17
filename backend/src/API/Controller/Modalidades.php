83b6eef96a028ffb849c0da374e539fedf8dbcf9
marcoaureliorp <marcoaurelio.rp@hotmail.com>
2020-03-16 21:29:50 -0300

    Implementa API e Model de Modalidades

--- backend/src/API/Controller/Modalidades.php
+++ backend/src/API/Controller/Modalidades.php
@@ -3,12 +3,44 @@
 namespace App\API\Controller;
 
 use App\Model\DB\Modalidade;
+use Exception;
 
 class Modalidades {
 
-    public function listar() {
+    /**
+     * @param $dados
+     * @return bool|int
+     * @throws Exception
+     */
+    public function salvar($dados) {
         $modalidade = new Modalidade();
-        return $modalidade->listar();
+
+        return isset($dados['modalidade_id'])
+            ? $modalidade->alterar($dados)
+            : $modalidade->adicionar($dados);
+    }
+
+    /**
+     * @param $dados
+     * @return array
+     */
+    public function listar($dados) {
+        $modalidade = new Modalidade();
+
+        $modalidade_id = $dados[Modalidade::COL_MODALIDADE_ID] ?? null;
+        $ordem = $dados['ordem'] ?? null;
+        $limite = $dados['ordem'] ?? null;
+
+        return $modalidade->listar($modalidade_id, $ordem, $limite);
+    }
+
+    /**
+     * @param $dados
+     * @return bool
+     */
+    public function excluir($dados) {
+        $modalidade = new Modalidade();
+        return $modalidade->excluir($dados['modalidade_id']);
     }
 
 }
--- backend/src/Model/DB/Modalidade.php
+++ backend/src/Model/DB/Modalidade.php
@@ -3,7 +3,10 @@
 namespace App\Model\DB;
 
 use Blazar\Component\Dao\CRUDMysql;
+use Blazar\Core\Log;
 use Blazar\Core\Manifest;
+use Exception;
+use GUMP;
 
 class Modalidade extends CRUDMysql {
 
@@ -19,12 +22,108 @@ class Modalidade extends CRUDMysql {
         parent::__construct(Manifest::db("main_db"));
     }
 
-    public function listar() {
+    /**
+     * @param $dados
+     * @return bool|int
+     * @throws Exception
+     */
+    public function adicionar($dados) {
+
+        if (isset($dados[self::COL_MODALIDADE_ID])) {
+            throw new Exception("Index " . self::COL_MODALIDADE_ID . " não é permitido no método adicionar.");
+        }
+
+        $dados = $this->cleanStruct($dados);
+
+        $is_valid = GUMP::is_valid($dados, [
+            self::COL_NOME => 'required|max_len,255'
+        ]);
+
+        // Verifica se existe erros
+        if ($is_valid !== true) {
+            throw new Exception(implode("\r\n", $is_valid));
+        }
+
+        $retorno = 0;
+
         try {
-            $result = $this->read(self::TABELA, "*", null, [], null, null, null, null);
-            return $result;
-        } catch (\Exception $e) {
+            $retorno = $this->create(self::TABELA, $dados);
+        } catch (Exception $e) {
+            Log::e("Model Modalidade", $e->getMessage() . "\n" . $e->getTraceAsString(), false, self::TABELA);
+        }
+
+        return $retorno;
+    }
+
+    /**
+     * @param $dados
+     * @return bool|int
+     * @throws Exception
+     */
+    public function alterar($dados) {
+        $dados = $this->cleanStruct($dados);
+
+        $is_valid = GUMP::is_valid($dados, [
+            self::COL_NOME => 'required|max_len,255'
+        ]);
+
+        // Verifica se existe erros
+        if ($is_valid !== true) {
+            throw new Exception(implode("\r\n", $is_valid));
+        }
+
+        $retorno = 0;
+
+        $where_con = self::COL_MODALIDADE_ID . " = ?";
+        $where_val = [$dados[self::COL_MODALIDADE_ID]];
+
+        try {
+            $retorno = $this->update(self::TABELA, $dados, $where_con, $where_val);
+        } catch (Exception $e) {
+            Log::e("Model Modalidade", $e->getMessage() . "\n" . $e->getTraceAsString(), false, self::TABELA);
+        }
+
+        return $retorno;
+    }
+
+    /**
+     * @param null $modalidade_id
+     * @param null $ordem
+     * @param null $limite
+     * @return array
+     */
+    public function listar($modalidade_id = null, $ordem = null, $limite = null) {
+        $where_con = "1 = 1";
+        $where_val = [];
+
+        if (!empty($modalidade_id)) {
+            $where_con .= " AND " . self::COL_MODALIDADE_ID . " = ?";
+            $where_val[] = $modalidade_id;
+        }
+
+        $ordem = !empty($ordem) ? $ordem : self::COL_NOME . " ASC";
+
+        try {
+            return $this->read(self::TABELA, "*", $where_con, $where_val, null, null, $ordem, $limite);
+        } catch (Exception $e) {
+            Log::e("Model Modalidade", $e->getMessage() . "\n" . $e->getTraceAsString(), false, self::TABELA);
             return [];
+        }
+    }
+
+    /**
+     * @param $modalidade_id
+     * @return bool
+     */
+    public function excluir($modalidade_id) {
+        $where_con = self::COL_MODALIDADE_ID . " = ?";
+        $where_val = [$modalidade_id];
+
+        try {
+            return $this->delete(self::TABELA, $where_con, $where_val);
+        } catch (Exception $e) {
+            Log::e("Model Modalidade", $e->getMessage() . "\n" . $e->getTraceAsString(), false, self::TABELA);
+            return false;
         }
     }
 }
